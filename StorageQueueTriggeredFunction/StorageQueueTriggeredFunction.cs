eeca3bdd286de1a2e6643db5c15da881ca4d77ec
Gary Green <iamggreen@gmail.com>
2018-01-20 12:41:27 -0600

    Reorganize solution to support multiple function projects

--- StorageQueueTriggeredFunction/StorageQueueTriggeredFunction.cs
+++ StorageQueueTriggeredFunction/StorageQueueTriggeredFunction.cs
@@ -3,15 +3,15 @@ using Microsoft.Azure.WebJobs.Host;
 
 namespace AzureFunctionStorageQueueTriggered
 {
-    public static class Function1
+    public static class StorageQueueTriggeredFunction
     {
-        [FunctionName("Function1")]
+        [FunctionName("StorageQueueTriggeredFunction")]
         public static void Run(
             [QueueTrigger("myqueue-items", Connection = "AzureWebJobsStorage")]string myQueueItem,
             [Blob("incontainer/{queueTrigger}", System.IO.FileAccess.Read)]string inputBlob,
             TraceWriter log)
         {
-            log.Info("Function1 executing...");
+            log.Info("StorageQueueTriggeredFunction executing...");
             log.Info($"C# Queue trigger function processed: {myQueueItem}");
             log.Info($"Blob Contents: {inputBlob}");
 
